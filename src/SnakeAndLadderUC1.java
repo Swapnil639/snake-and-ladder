b74d1d8e38a3845814e9beaa12170ed2fba803a8
Swapnil639 <swapnilpatil631997@gmail.com>
2022-05-24 22:10:39 +0530

    start to play

--- src/SnakeAndLadderUC1.java
+++ src/SnakeAndLadderUC1.java
@@ -1,7 +1,7 @@
 public class SnakeAndLadderUC1 {
     public static void main(String[] args) {
-        int player = 0;
-        System.out.println("Single Player at Start position 0 ");
+        int position = 0;
+        System.out.println("Single Player at Start position :"+position);
     }
 }
 
